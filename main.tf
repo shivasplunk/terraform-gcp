810372d5cbef6a647017513ef550fd5e25026a9c
Shiva <shivasplunk1991@gmail.com>
2026-03-04 00:24:43 +0530

    Use variable for GCP credentials

--- main.tf
+++ main.tf
@@ -1,24 +1,10 @@
-provider "google" {
-  credentials = file("gcp-key.json")
-  project     = "terraform-project-489115"
-  region      = "us-central1"
+variable "gcp_credentials" {
+  type      = string
+  sensitive = true
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-vm"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-    }
-  }
+provider "google" {
+  credentials = var.gcp_credentials
+  project     = "terraform-project-489115"
+  region      = "us-central1"
 }
