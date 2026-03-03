dbbfff3fc4e54ca5cbd5bf84e59a1d4f187c1863
Shiva <shivasplunk1991@gmail.com>
2026-03-04 01:01:41 +0530

    Add VM resource to Terraform config

--- main.tf
+++ main.tf
@@ -8,3 +8,20 @@ provider "google" {
   project     = "terraform-project-489115"
   region      = "us-central1"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
